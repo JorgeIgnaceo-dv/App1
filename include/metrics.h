cce46166266d314cf79826bacf5c13faa99b57c3
Diego Subiabre Onate <dsubiabreonate@gmail.com>
2025-03-27 22:13:49 -0300

    Distribucion de metricas a desarrollar

--- include/metrics.h
+++ include/metrics.h
@@ -13,16 +13,16 @@ typedef struct {
 } Metricas;
 
 // Declaraciones de funciones métricas
-char* pms(int* size, Order* orders);   // Pizza más vendida
-char* pls(int* size, Order* orders);   // Pizza menos vendida
-char* dms(int* size, Order* orders);   // Día con más ventas en dinero
-char* dls(int* size, Order* orders);   // Día con menos ventas en dinero
-char* dmsp(int* size, Order* orders);  // Día con más pizzas vendidas
-char* dlsp(int* size, Order* orders);  // Día con menos pizzas vendidas
-char* apo(int* size, Order* orders);   // Promedio de pizzas por orden
-char* apd(int* size, Order* orders);   // Promedio de pizzas por día
-char* ims(int* size, Order* orders);   // Ingrediente más vendido
-char* hp(int* size, Order* orders);    // Histograma por categoría
+char* pms(int* size, Order* orders);   // Pizza más vendida(Jorge)
+char* pls(int* size, Order* orders);   // Pizza menos vendida(Carlos)
+char* dms(int* size, Order* orders);   // Día con más ventas en dinero(Carlos)
+char* dls(int* size, Order* orders);   // Día con menos ventas en dinero(Jorge)
+char* dmsp(int* size, Order* orders);  // Día con más pizzas vendidas(Diego)
+char* dlsp(int* size, Order* orders);  // Día con menos pizzas vendidas(Jorge)
+char* apo(int* size, Order* orders);   // Promedio de pizzas por orden(Carlos)
+char* apd(int* size, Order* orders);   // Promedio de pizzas por día(Diego)
+char* ims(int* size, Order* orders);   // Ingrediente más vendido(Pendiente)
+char* hp(int* size, Order* orders);    // Cantidad de pizzas por categoria vendidas(Diego)
 
 #endif
 
